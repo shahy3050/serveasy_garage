f3f82744d0063dd2f3d3d3517f20b6db24a4cb16
Yash Shah <shahy3050@gmail.com>
2020-02-05 19:45:50 +0530

    Login Authentication Done

--- app/src/main/java/com/example/serveasy_garage/LoginActivity.java
+++ app/src/main/java/com/example/serveasy_garage/LoginActivity.java
@@ -1,16 +1,123 @@
 package com.example.serveasy_garage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
 
+    private FirebaseAuth mAuth;
+    private EditText email_et, password_et;
+    private Button login_bt;
+
+
+    public void bindViews() {
+        email_et = findViewById(R.id.email_edit_text);
+        password_et = findViewById(R.id.password_edit_text);
+        login_bt = findViewById(R.id.login_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        Log.d("serveasy", "User :" + user);
+    }
+
+
+    public void attemptLogin() {
+        email_et.setError(null);
+        password_et.setError(null);
+
+        String email = email_et.getText().toString();
+        String password = password_et.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(password)) {
+            password_et.setError("Password is Empty!!! ");
+            focusView = password_et;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            email_et.setError("Email ID is Empty");
+            focusView = email_et;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            email_et.setError("Enter a valid Email ID");
+            focusView = email_et;
+            cancel = true;
+        }
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            logIn();
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        // You can add more checking logic here.
+        return email.contains("@");
+    }
+
+
+    public void logIn() {
+
+        final String email = email_et.getText().toString();
+        final String password = password_et.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Log.d("serveasy", "Sign in Successful");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Log.d("serveasy", "USER IS: " + user);
+                } else {
+                    Log.d("serveasy", "Failure in Sign In" + task.getException());
+                    Toast.makeText(LoginActivity.this, "Cant Log In", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        bindViews();
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        attemptLogin();
+
+
     }
+
+
+    //Sign In Method For Existing User
+    /**/
+
+
 }
